72418097a8f6ddd373ef93df2baa03a723cdabb3
bmarquismarkail <bmmgreen@gmail.com>
2017-02-14 17:04:27 -0600

    adding cs files and manifest

---
+++ ModConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SB_PotC
+{
+
+    public class ModConfig
+    {
+        public int witnessBonus { get; set; }
+        public int storytellerBonus { get; set; }
+        public int ujamaaBonus { get; set; }
+        public int umojaBonusFestival { get; set; }
+        public int umojaBonusMarry { get; set; }
+        public int umojaBonus { get; set; }
+        public int ujimaBonusStore { get; set; }
+        public int ujimaBonus { get; set; }
+        public int kuumbaBonus { get; set; }
+
+        public ModConfig()
+        {
+            witnessBonus = 4;
+            storytellerBonus = 4;
+            ujamaaBonus = 4;
+            umojaBonus = 10;
+            umojaBonusFestival = 16;
+            umojaBonusMarry = 240;
+            ujimaBonusStore = 20;
+            ujimaBonus = 2;
+            kuumbaBonus = 2;
+        }
+    }
+}
