d64d0f185f3e7b7f97d297764d47902ded2d829a
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-25 20:37:16 -0400

    bump version for release

--- PartOfTheCommunity/Properties/AssemblyInfo.cs
+++ PartOfTheCommunity/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("58913321-94a9-4b11-95c4-fabaef78ae76")]
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
